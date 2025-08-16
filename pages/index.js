47e31578d6e7d54b4f40340db43c94994acb5496
Shyam4508 <145720094+Shyam4508@users.noreply.github.com>
2025-08-17 02:32:08 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 My E-commerce Website</h1>
+      <p>If you can see this, Vercel build is working!</p>
+    </div>
+  );
+}
