94a0725fd8e46b58bc10f473c5509099fe43fc7f
Shyam4508 <145720094+Shyam4508@users.noreply.github.com>
2025-08-17 02:30:51 +0530

    Create app page.js

---
+++ app/app page.js
@@ -0,0 +1,9 @@
+// app/page.js
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to My E-commerce</h1>
+      <p>Built with Next.js 13 App Router 🚀</p>
+    </main>
+  );
+}
